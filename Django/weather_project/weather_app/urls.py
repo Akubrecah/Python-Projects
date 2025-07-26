9f520d0afb11233c3ea920532206f79f3083c162
Akubrecah <poweldayck@gmail.com>
2025-07-27 01:34:24 +0300

    .

--- Django/weather_project/weather_app/urls.py
+++ Django/weather_project/weather_app/urls.py
@@ -1,6 +1,8 @@
+# weather_app/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.weather_dashboard, name='dashboard'),
+    path('', views.index, name='home'),  # Main weather page
+    path('forecast/', views.forecast, name='forecast'),  # Example additional route
 ]
