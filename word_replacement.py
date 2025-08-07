08bf117c3b047e14aad78da07f4caccd015c40c3
Akubrecah <poweldayck@gmail.com>
2025-08-07 23:59:36 +0300

    modified:   word_replacement.py

--- word_replacement.py
+++ word_replacement.py
@@ -1,3 +1,9 @@
 def replace_word():
+    
     str = "Hi am Akubrecah and hi hi hi"
-    replace_to_word = ""
+    replace_to_word = input("Enter the word to replcae: ")
+    word_replacement = input("Enter the word replacement: ")
+
+    print(str.replace(replace_to_word, word_replacement))
+
+replace_word()
